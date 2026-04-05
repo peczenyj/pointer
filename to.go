fd91225b39143a6fa22e70f8f47f2d18caca3069
Tiago Peczenyj <tiago.peczenyj@gmail.com>
2026-04-05 18:25:07 +0200

    feat: add go1.26+ implementation using new builtin

--- to.go
+++ to.go
@@ -1,9 +1,14 @@
-// The idea behing this package is to help handle with pointers in very common tasks.
+//go:build go1.26
+// +build go1.26
+
+// The idea behind this package is to help handle with pointers in very common tasks.
 //
 // It exposes just a single public function [To]
 package pointer
 
 // To returns a pointer to an object of a given type.
+//
+// On Go 1.26+, this is a simple wrapper around the built-in new function.
 //
 //	p := pointer.To(64) // assume type is int
 //
@@ -11,5 +16,7 @@ package pointer
 //
 // Outputs: "return a pointer to int with value 64"
 func To[V any](object V) *V {
-	return &object
+	p := new(V)
+	*p = object
+	return p
 }
